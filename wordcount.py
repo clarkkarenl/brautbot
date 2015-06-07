992fed0c3b7c021a6023aaa0c7a42832e72780a8
Karen Clark <clarkkarenl@gmail.com>
2015-06-07 13:53:39 -0700

    Fix for sorting, adding output file for example
    Fixed sorting by using itemgetter.
    Added the output file it created, for later reference.

--- wordcount.py
+++ wordcount.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+from operator import itemgetter
+
 # open the source file containing all poems
 with open('C:\Users\karen\poems_for_wordcount.txt', 'r+') as sourcefile:
     wordcount = {}
@@ -10,10 +12,14 @@ with open('C:\Users\karen\poems_for_wordcount.txt', 'r+') as sourcefile:
         else:
             wordcount[word] += 1
 
+# reverse sort on number of occurrences (v)
+# TODO also non-reverse (alpha) sort on k -  do I care about that?
+x = sorted(wordcount.items(), key=itemgetter(1), reverse=1)
+
 # open the outfile where the lines will be written
 with open('C:\Users\karen\poem_words_out.txt', 'w+') as outfile:
     # write each line to the outfile
-    for k, v in wordcount.items():
+    for k, v in x:
         outfile.write(str(k) + " " + str(v) + "\n")
 
 # close the files
