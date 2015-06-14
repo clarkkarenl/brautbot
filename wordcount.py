3b673b6b144cd5b39755dcada60a993616fb3658
George Song <george@55minutes.com>
2015-06-14 06:52:54 -0700

    Start refactoring to make the code more testable

--- wordcount.py
+++ wordcount.py
@@ -8,25 +8,32 @@ _path = os.path.abspath(os.path.dirname(__file__))
 SOURCE = os.path.join(_path, 'poems_for_wordcount.txt')
 DESTINATION = os.path.join(_path, 'poem_words_out.txt')
 
-# open the source file containing all poems
-# with open() closes after, no need to do that separately
-with open(SOURCE, 'r+') as sourcefile:
-    wordcount = {}
-    # count occurrence of each word, lowercase to avoid Hello and hello being
-    # distinct
-    for word in sourcefile.read().lower().split():
-        if word not in wordcount:
-            wordcount[word] = 1
-        else:
-            wordcount[word] += 1
 
-# first sort to get k by alpha
-y = sorted(wordcount.items(), key=itemgetter(0))
-# then reverse sort on number of occurrences (v) to get list in desc order
-x = sorted(y, key=itemgetter(1), reverse=1)
+def main():
 
-# open the outfile where the lines will be written
-with open(DESTINATION, 'w+') as outfile:
-    # write each line to the outfile
-    for k, v in x:
-        outfile.write(str(k) + " " + str(v) + "\n")
+    # open the source file containing all poems
+    # with open() closes after, no need to do that separately
+    with open(SOURCE, 'r+') as sourcefile:
+        wordcount = {}
+        # count occurrence of each word, lowercase to avoid Hello and hello
+        # being distinct
+        for word in sourcefile.read().lower().split():
+            if word not in wordcount:
+                wordcount[word] = 1
+            else:
+                wordcount[word] += 1
+
+    # first sort to get k by alpha
+    y = sorted(wordcount.items(), key=itemgetter(0))
+    # then reverse sort on number of occurrences (v) to get list in desc order
+    x = sorted(y, key=itemgetter(1), reverse=1)
+
+    # open the outfile where the lines will be written
+    with open(DESTINATION, 'w+') as outfile:
+        # write each line to the outfile
+        for k, v in x:
+            outfile.write(str(k) + " " + str(v) + "\n")
+
+
+if __name__ == '__main__':
+    main()
