e8d80b104f947ae01303132403a5eaa02449d552
George Song <george@55minutes.com>
2015-06-14 07:50:08 -0700

    Combine everything into main
    Since our functions are one liners, it's a bit silly to have them separated out.

--- wordcount.py
+++ wordcount.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
+from __future__ import print_function
+
 from collections import Counter
 from operator import itemgetter
 import os
-import pytest
 
 
 _path = os.path.abspath(os.path.dirname(__file__))
@@ -11,43 +12,14 @@ SOURCE = os.path.join(_path, 'poems_for_wordcount.txt')
 DESTINATION = os.path.join(_path, 'poem_words_out.txt')
 
 
-def count_words(word_list):
-    return Counter(word_list)
-
-
-def sort_word_count(word_dict):
-    return sorted(word_dict.items(), key=itemgetter(1), reverse=True)
-
-
 def main():
-    # open the source file containing all poems
-    # with open() closes after, no need to do that separately
-    with open(SOURCE, 'r+') as sourcefile:
-        # count occurrence of each word, lowercase to avoid Hello and hello
-        # being distinct
-        word_count = count_words(sourcefile.read().lower().split())
-
-    x = sort_word_count(word_count)
-
-    # open the outfile where the lines will be written
-    with open(DESTINATION, 'w+') as outfile:
-        # write each line to the outfile
-        for k, v in x:
-            outfile.write(str(k) + " " + str(v) + "\n")
-
-
-@pytest.fixture
-def word_list():
-    return 'you watch the brown fox jumped over the fence'.split()
-
-
-def test_count_words(word_list):
-    assert count_words(word_list)['the'] == 2
-
-
-def test_sort_word_count(word_list):
-    word_count = count_words(word_list)
-    assert sort_word_count(word_count)[0][0] == 'the'
+    with open(SOURCE, 'rb') as source, open(DESTINATION, 'wb') as destination:
+        word_counts = Counter(source.read().lower().split())
+        sorted_count_list = sorted(
+            word_counts.items(), key=itemgetter(1), reverse=True
+        )
+        for item in sorted_count_list:
+            print("{} {}".format(*item), file=destination)
 
 
 def test_output():
